1b44b3eea1c9779be9c2905698c2b1215a1915d6
sophia-jihye <jihyeparkk@naver.com>
2020-07-07 21:49:30 +0900

    token2indices bugfix

--- source/DependencyGraphHandler.py
+++ source/DependencyGraphHandler.py
@@ -40,46 +40,46 @@ class DependencyGraphHandler:
                 
         return [token2tagdep[token] for token in shortest_path]
     
-    def next_focused_tokens(self, current_tokens, token2idx, nodes, dep_rel):
-        focused_tokens = set()
-        for current_token in current_tokens:
-            indices = token2idx[current_token]
-            for current_token_idx in indices:
-                if nodes[current_token_idx].dep == dep_rel:
-                    focused_tokens.add(nodes[nodes[current_token_idx].governor].token)
-                child_nodes = [nodes[i] for i in range(len(nodes)) if nodes[i].governor==current_token_idx]
-                focused_tokens.update([child_node.token for child_node in child_nodes if child_node.dep == dep_rel])
-        return focused_tokens
+    def next_focused_token_indices(self, current_token_indices, nodes, dep_rel):
+        focused_token_indices = set()
+        for current_token_idx in current_token_indices:
+            if nodes[current_token_idx].dep == dep_rel:
+                focused_token_indices.add(nodes[nodes[current_token_idx].governor].idx)
+            child_nodes = [nodes[i] for i in range(len(nodes)) if nodes[i].governor==current_token_idx]
+            focused_token_indices.update([child_node.idx for child_node in child_nodes if child_node.dep == dep_rel])
+        return focused_token_indices
 
-    def next_compound_token(self, current_token, token2idx, nodes):
-        compound_child_nodes = [nodes[i] for i in range(len(nodes)) if nodes[i].governor in token2idx[current_token] and nodes[i].dep.startswith('compound')]
+    def next_compound_token_idx(self, current_token_idx, nodes):
+        compound_child_nodes = [nodes[i] for i in range(len(nodes)) if nodes[i].governor == current_token_idx and nodes[i].dep.startswith('compound')]
         
-        if len(compound_child_nodes) > 0: return compound_child_nodes[0].token
+        if len(compound_child_nodes) > 0: return compound_child_nodes[0].idx
         return None
     
-    def compound(self, new_targets, token2idx, nodes):
+    def compound(self, new_targets, indices_for_new_targets, nodes):
         to_be_deleted, to_be_added = set(), set()
-        for target in new_targets:
-            compound_tokens = list()
-
-            focused_token = target
+        for target_idx in indices_for_new_targets:
+            
+            compound_token_indices = list()
+            focused_token_idx = target_idx
             while True:
-                focused_token = self.next_compound_token(focused_token, token2idx, nodes)
-                if focused_token is None or focused_token in compound_tokens: break
-                compound_tokens.append(focused_token)
-            compound_tokens.append(target)
+                focused_token_idx = self.next_compound_token_idx(focused_token_idx, nodes)
+                if focused_token_idx is None or focused_token_idx in compound_token_indices: break
+                compound_token_indices.append(focused_token_idx)
+            compound_token_indices.append(target_idx)
 
-            if len(compound_tokens) > 1:
-                to_be_deleted.add(target)
-                to_be_added.add(' '.join(compound_tokens))
+            if len(compound_token_indices) > 1:
+                to_be_deleted.add(nodes[target_idx].token)
+                to_be_added.add(' '.join([nodes[i].token for i in compound_token_indices]))
         for item in to_be_deleted:
             new_targets.remove(item)
         for item in to_be_added:
             new_targets.add(item)
     
-    def extract_targets_using_pattern(self, token2idx, nodes, opinion_words, dep_rels):
-        focused_tokens = set(opinion_words)
+    def extract_targets_using_pattern(self, token2indices, nodes, opinion_words, dep_rels):
+        focused_token_indices = set([item for sublist in [token2indices[token] for token in opinion_words] for item in sublist])
         for i in range(len(dep_rels)):
-            focused_tokens = self.next_focused_tokens(focused_tokens, token2idx, nodes, dep_rels[i])
-        self.compound(focused_tokens, token2idx, nodes)
-        return focused_tokens
+            focused_token_indices = self.next_focused_token_indices(focused_token_indices, nodes, dep_rels[i])
+        
+        new_targets = set([nodes[i].token for i in focused_token_indices])
+        self.compound(new_targets, focused_token_indices, nodes)
+        return new_targets
